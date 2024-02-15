9f238da8ad2b658b07b1599c92c75240ed6ce4c2
kavinlong4457 <159468746+kavinlong4457@users.noreply.github.com>
2024-02-15 10:44:37 +0700

    Update predictor.py

--- roop/predictor.py
+++ roop/predictor.py
@@ -1,9 +1,6 @@
 import threading
 import numpy
-import opennsfw2
 from PIL import Image
-from keras import Model
-
 from roop.typing import Frame
 
 PREDICTOR = None
@@ -11,12 +8,7 @@ THREAD_LOCK = threading.Lock()
 MAX_PROBABILITY = 0.85
 
 
-def get_predictor() -> Model:
-    global PREDICTOR
-
-    with THREAD_LOCK:
-        if PREDICTOR is None:
-            PREDICTOR = opennsfw2.make_open_nsfw_model()
+def get_predictor():
     return PREDICTOR
 
 
@@ -27,10 +19,6 @@ def clear_predictor() -> None:
 
 
 def predict_frame(target_frame: Frame) -> bool:
-    image = Image.fromarray(target_frame)
-    image = opennsfw2.preprocess_image(image, opennsfw2.Preprocessing.YAHOO)
-    views = numpy.expand_dims(image, axis=0)
-    _, probability = get_predictor().predict(views)[0]
     return False
 
 
@@ -39,5 +27,4 @@ def predict_image(target_path: str) -> bool:
 
 
 def predict_video(target_path: str) -> bool:
-    _, probabilities = opennsfw2.predict_video_frames(video_path=target_path, frame_interval=100)
     return False
